ac1d5c32fde635e014fbacc60692f85784954d2a
Pattharamon D. <pattharamon.d@ku.th>
2024-09-01 12:51:10 +0700

    displays end_date

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('end date', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
