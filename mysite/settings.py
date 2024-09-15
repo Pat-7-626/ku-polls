5bc85edb822c088ab51ffa82d0696f1b5411f094
Pattharamon D. <pattharamon.d@ku.th>
2024-09-15 10:16:00 +0700

    go to index when 404 occurred

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'polls.middleware.LogUserLoginMiddleware',
+    'polls.middleware.Custom404Middleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
